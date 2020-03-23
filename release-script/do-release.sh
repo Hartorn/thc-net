d9cca6757ea4bfaaaddc864da0c682e113d86455
Hartorn <hartorn.github@gmail.com>
2020-03-23 16:42:56 +0100

    chore: minor fixes for release

--- release-script/do-release.sh
+++ release-script/do-release.sh
@@ -29,7 +29,7 @@ git tag v${version}
 
 # Build release
 echo "Building latest build"
-docker run --rm -v ${PWD}:/work -w /work thc-net:latest poetry build
+docker run --rm -v ${PWD}/thc-net:/work -w /work thc-net:latest poetry build
 
 echo "Merging into develop and master"
 git checkout master
@@ -48,10 +48,10 @@ docker run -v ${PWD}:/work -w /work --entrypoint "" release-changelog:latest con
 
 # Build release
 echo "Building latest build"
-docker run --rm -v ${PWD}:/work -w /work thc-net:latest poetry build
+docker run --rm -v ${PWD}/thc-net:/work -w /work thc-net:latest poetry build
 # Build release
 echo "Publishing latest build"
-docker run --rm -v ${PWD}:/work -w /work thc-net:latest poetry publish -u ${pipyUser} -p ${pipyPassword}
+docker run --rm -v ${PWD}/thc-net:/work -w /work thc-net:latest poetry publish -u ${pipyUser} -p ${pipyPassword}
 
 echo "Deleting release branch"
 git checkout develop

70ce40028db5b96ff22ccc6de3f1651b3e2cf314
Bazire Houssin <bazire.houssin@dreamquark.com>
2020-06-15 23:45:33 +0200

    chore: building tensorflow for 10.2

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+cd tensorflow/
+
+TMP=/tmp \
+bazel build \
+--local_ram_resources=HOST_RAM*0.5 \
+--local_cpu_resources=HOST_CPUS-1 \
+//tensorflow/tools/pip_package:build_pip_package \
+--config=noaws \
+--config=nogcp \
+--config=v2 \
+--config=mkl \
+--config=opt
+
+
+mkdir -p /work/result_tf
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package /work/result_tf
